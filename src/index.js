36c3836319b2b0a2d203eb805e24b74646530303
root <root@localhost.localdomain>
2019-08-11 20:59:32 -0300

    init backend

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 const express = require('express')
 const app = express()
 const mongoose = require('mongoose')
+const cors = require('cors')
 const routes = require('./routes')
 const port = 3333;
 
 mongoose.connect('mongodb+srv://joao:200*technology@cluster0-gn4bg.mongodb.net/test?retryWrites=true&w=majority', {
     useNewUrlParser: true
 })
+app.use(cors())
 app.use(routes)
 app.listen(port, () => {
     console.log(`Server is runing in ${port}`)
